1d5cfc718dfd8f988a65eb18285962694f109160
Sven Arweiler <dev@sba.io>
2019-12-25 19:28:20 +0100

    use helpers in core

--- bragi_core.lua
+++ bragi_core.lua
@@ -43,11 +43,14 @@ local bpm_to_sec,
       tab_shift
       
 
--- Euclidean rhythm (http://en.wikipedia.org/wiki/Euclidean_Rhythm)
--- @param p : number of pulses
--- @param s : total number of steps
+-- some helper functions
+
+local helper = {}
+helper.tab = {}
+
+
 -- stolen from https://github.com/monome/norns/blob/master/lua/lib/er.lua
-er_gen = function(p, s)
+helper.er_gen = function(p, s)
   -- results array, intially all zero
   local r = {}
   for i=1,s do r[i] = false end
@@ -62,32 +65,29 @@ er_gen = function(p, s)
   return r
 end
 
-
-bpm_to_sec = function(bpm)
+helper.bpm_to_sec = function(bpm)
   return 60 / bpm / 4
 end
 
+helper.tab.shift_left = function(t)
+  local t_clone = helper.tab.clone(t)
+  table.insert(t_clone, #t_clone + 1, t_clone[1])
+  table.remove(t_clone, 1)
+  return t_clone
+end
 
-tab_shift = function(t)
-  local t_clone = tab_clone(t)
+helper.tab.shift_right = function(t)
+  local t_clone = helper.tab.clone(t)
   table.insert(t_clone, 1, t_clone[#t_clone])
   table.remove(t_clone, #t_clone)
   return t_clone
 end
 
-
-tab_clone = function(t)
+helper.tab.clone = function(t)
   return {table.unpack(t)}
 end
 
-
-shift_seq = function(seq)
-  local seq_clone = tab_clone(seq)
-  return tab_shift(seq_clone)
-end
-
-
-debug_tab = function(tab)
+helper.tab.debug = function(tab)
   print("---")
   for k,v in ipairs(tab) do
     print(k .. ": " .. tostring(v))
@@ -99,7 +99,7 @@ step = function(c)
   if state.seqs[c].sequence[c] == true then
     output[c]()
   end
-  state.seqs[c].sequence = shift_seq(state.seqs[c].sequence)
+  state.seqs[c].sequence = helper.tab.shift_left(state.seqs[c].sequence)
 end
 
 
@@ -107,10 +107,10 @@ end
 function init()
   for i=1,#state.seqs do
     output[i].action = { to(5,0), to(0, 0.25) }
-    state.seqs[i].sequence = er_gen(state.seqs[i].pulses, state.seqs[i].steps)
+    state.seqs[i].sequence = helper.er_gen(state.seqs[i].pulses, state.seqs[i].steps)
     state.seqs[i].metro = metro.init{
       event = function() step(i) end,
-      time = bpm_to_sec(state.seqs[i].bpm),
+      time = helper.bpm_to_sec(state.seqs[i].bpm),
       count = CONFIG.SEQ.STEPCOUNT
     }
   end
